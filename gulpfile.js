c14f417ed860f70e9d57d2e0616b90d69509a4ea
tpiecora <tpiecora@gmail.com>
2015-05-30 21:18:28 +0700

    setting up coveralls

--- gulpfile.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by michaelpiecora on 5/30/15.
- */
-var coveralls = require('gulp-coveralls');
-
-gulp.task('coveralls', function () {
-  return gulp.src('./path/to/lcov/file.lcov')
-    .pipe(coveralls());
-});
